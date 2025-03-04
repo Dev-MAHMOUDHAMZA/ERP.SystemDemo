5dea230b9bd28dbc5d8c884e6fc48d2b8dbb060f
Mahmoud Hamza <dev.mahmoudhamza@gmail.com>
2025-03-04 13:40:53 +0200

    Add ConvertDecimalToWords

--- ERP.Web/Controllers/HomeController.cs
+++ ERP.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-using ERP.Web.Models;
+﻿using ERP.Web.Models;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace ERP.Web.Controllers;
 public class HomeController : Controller
@@ -14,7 +16,30 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+
+        decimal number = 2545.44m;
+        string textDecimalToWords = ConvertDecimalToWords(number, "جنيه مصري", "قرش");
+        return View((object)textDecimalToWords);
+    }
+
+    public static string ConvertDecimalToWords(decimal number, string mainCurrency = "", string subCurrency = "")
+    {
+        var wholePart = (int)number;
+        var decimalPart = (int)Math.Round((number - wholePart) * 100);
+
+        var wholeText = wholePart.ToWords(new CultureInfo("ar"));
+        var decimalText = decimalPart > 0 ? decimalPart.ToWords(new CultureInfo("ar")) : "";
+
+        var result = string.IsNullOrWhiteSpace(mainCurrency) ? wholeText : $"{wholeText} {mainCurrency}";
+
+        if (decimalPart > 0)
+        {
+            result += string.IsNullOrWhiteSpace(subCurrency)
+                ? $" فاصلة {decimalText}"
+                : $" و {decimalText} {subCurrency}";
+        }
+
+        return result;
     }
 
     public IActionResult Privacy()
