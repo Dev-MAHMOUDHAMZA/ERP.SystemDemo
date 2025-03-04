0e56cec25d112c6c1037890c3a856e0478ec4662
Mahmoud Hamza <dev.mahmoudhamza@gmail.com>
2025-03-04 09:19:28 +0200

    Edit FormAjax

--- ERP.Web/Common/FormAjax.cs
+++ ERP.Web/Common/FormAjax.cs
@@ -3,13 +3,27 @@ using Microsoft.AspNetCore.Mvc.ActionConstraints;
 
 namespace ERP.Web.Common;
 
+//public class FormAjax : ActionMethodSelectorAttribute
+//{
+//    public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+//    {
+//        var request = routeContext.HttpContext.Request;
+//        var isAjax = request.Headers["x-requested-with"] == "XMLHttpRequest";
+
+//        return isAjax;
+//    }
+//}
+
 public class FormAjax : ActionMethodSelectorAttribute
 {
+    private const string HeaderKey = "x-requested-with";
+    private const string AjaxValue = "XMLHttpRequest";
+
     public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
     {
         var request = routeContext.HttpContext.Request;
-        var isAjax = request.Headers["x-requested-with"] == "XMLHttpRequest";
 
-        return isAjax;
+        return request.Headers.TryGetValue(HeaderKey, out var headerValue) &&
+               string.Equals(headerValue, AjaxValue, StringComparison.OrdinalIgnoreCase);
     }
 }
